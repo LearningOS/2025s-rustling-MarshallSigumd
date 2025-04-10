753016ee1f5c1d86b513ad6eb41d26b5a95a5035
root <root@DESKTOP-2JOPMHO.>
2025-04-10 14:21:39 +0800

    update

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -1,8 +1,8 @@
 /*
-	heap
-	This question requires you to implement a binary heap function
+    heap
+    This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
+
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,9 +37,26 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
-    }
+        // 增加计数并在需要时扩展 items 向量
+        self.count += 1;
+        if self.count >= self.items.len() {
+            self.items.push(value);
+        } else {
+            self.items[self.count] = value;
+        }
 
+        // 向上调整堆
+        let mut idx = self.count;
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent]) {
+                self.items.swap(idx, parent);
+                idx = parent;
+            } else {
+                break;
+            }
+        }
+    }
     fn parent_idx(&self, idx: usize) -> usize {
         idx / 2
     }
@@ -57,8 +74,23 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+
+        if right <= self.count {
+            // 如果右子节点存在，比较左右子节点
+            if (self.comparator)(&self.items[left], &self.items[right]) {
+                left
+            } else {
+                right
+            }
+        } else if left <= self.count {
+            // 如果只有左子节点
+            left
+        } else {
+            // 没有子节点
+            idx
+        }
     }
 }
 
@@ -83,9 +115,33 @@ where
 {
     type Item = T;
 
-    fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        fn next(&mut self) -> Option<T> {
+        if self.count == 0 {
+            return None;
+        }
+
+        // 保存堆顶元素
+        let ret = std::mem::replace(&mut self.items[1], T::default());
+
+        // 将最后一个元素移到堆顶
+        if self.count > 1 {
+            self.items[1] = std::mem::replace(&mut self.items[self.count], T::default());
+        }
+        self.count -= 1;
+
+        // 向下调整堆
+        let mut idx = 1;
+        while self.children_present(idx) {
+            let child = self.smallest_child_idx(idx);
+            if (self.comparator)(&self.items[child], &self.items[idx]) {
+                self.items.swap(child, idx);
+                idx = child;
+            } else {
+                break;
+            }
+        }
+
+        Some(ret)
     }
 }
 
